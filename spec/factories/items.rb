4d8b0f84aecd8c93bef170d51a96b932bc1475b2
uni910 <megumioda27@gmail.com>
2022-08-21 04:10:36 +0900

    テストコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+    name {Faker::Lorem.sentence}
+    explanation {Faker::Lorem.sentence}
+    price {Faker::Lorem.sentence}
+    category_id {Faker::Lorem.sentence}
+    condition_id {Faker::Lorem.sentence}
+    send_price_id {Faker::Lorem.sentence}
+    send_day_id {Faker::Lorem.sentence}
+    prefecture_id {Faker::Lorem.sentence}
+    association :user
   end
 end
