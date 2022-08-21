31306bb0d4380f297728b6a2da8a3b4831650413
uni910 <megumioda27@gmail.com>
2022-08-21 17:49:33 +0900

    レビュー後修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -28,27 +28,27 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Explanation can't be blank")
       end     
       it "カテゴリーがないと商品は出品できない" do
-        @item.category_id = ''
+        @item.category_id = '1'
         @item.valid?
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
       it "状態がないと商品は出品できない" do
-        @item.condition_id = ''
+        @item.condition_id = '1'
         @item.valid?
         expect(@item.errors.full_messages).to include("Condition can't be blank")
       end     
       it "配送料の負担がないと商品は出品できない" do
-        @item.send_price_id = ''
+        @item.send_price_id = '1'
         @item.valid?
         expect(@item.errors.full_messages).to include("Send price can't be blank")
       end
       it "配送元の地域がないと商品は出品できない" do
-        @item.prefecture_id = ''
+        @item.prefecture_id = '1'
         @item.valid?
         expect(@item.errors.full_messages).to include("Prefecture can't be blank")
       end     
       it "発送までの日数がないと商品は出品できない" do
-        @item.send_day_id = ''
+        @item.send_day_id = '1'
         @item.valid?
         expect(@item.errors.full_messages).to include("Send day can't be blank")
       end
@@ -57,10 +57,25 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
-      it "価格が¥300~¥9,999,999の間以外だと商品は出品できない" do
+      it "価格が¥300以下だと商品は出品できない" do
         @item.price = '100'
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+      it "価格が¥9,999,999以上だと商品は出品できない" do
+        @item.price = '10000000'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+      end
+      it "価格が数字以外だと商品は出品できない" do
+        @item.price = '価格'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+      it 'userが紐付いていないと保存できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('User must exist')
       end
     end
   end
