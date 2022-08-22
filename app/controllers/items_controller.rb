e2228fc80986469e342256d2dbc632a4f78cbde2
uni910 <megumioda27@gmail.com>
2022-08-22 15:54:53 +0900

    商品詳細表示機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,9 @@
 class ItemsController < ApplicationController
 
-  before_action :authenticate_user!,except: :index #, :show]
+  before_action :authenticate_user!,except: [:index, :show]
 
   def index
-    @items = Item.includes(:user).order("created_at DESC")
+    @items = Item.order("created_at DESC")
   end
 
   def new
@@ -19,9 +19,9 @@ class ItemsController < ApplicationController
     end
   end
 
-  # def show
-  #   @item = Item.find(params[:id])
-  # end
+  def show
+    @item = Item.find(params[:id])
+  end
 
   # def edit
   #   @item = Item.find(params[:id])
