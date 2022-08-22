9d5feb82630cd6a55a83b045ea743eb885fc8423
uni910 <megumioda27@gmail.com>
2022-08-22 13:49:08 +0900

    商品詳細表示機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
 
-  before_action :authenticate_user!,except: :index
+  before_action :authenticate_user!,except: [:index, :show]
 
   def index
     @item = Item.new
@@ -18,6 +18,10 @@ class ItemsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def show
+    @item = Item.find(params[:id])
   end
 
   private
